e622d51c396d4243f3a769379fbb02836372cddf
Rowan Gudmundsson <rowan.gudmunds@gmail.com>
2024-04-18 16:39:08 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/** @type {import('eslint').} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
+
+module.exports = config;
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  singleQuote: true,
+  parser: 'typescript',
+};
+
+module.exports = config;
